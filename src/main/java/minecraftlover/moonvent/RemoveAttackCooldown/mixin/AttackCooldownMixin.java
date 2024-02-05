35c6c04cca409b91c128b1c71d54af591ef417ff
Nikita Kurkurin <nkl@reshape.team>
2024-02-05 21:30:41 +0100

    master
    1) Reduce attack speed for axes;

--- src/main/java/minecraftlover/moonvent/RemoveAttackCooldown/mixin/AttackCooldownMixin.java
+++ src/main/java/minecraftlover/moonvent/RemoveAttackCooldown/mixin/AttackCooldownMixin.java
@@ -1,7 +1,9 @@
 package minecraftlover.moonvent.RemoveAttackCooldown.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -9,7 +11,11 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(PlayerEntity.class)
 public class AttackCooldownMixin {
 	@ModifyReturnValue(method = "getAttackCooldownProgress", at = @At("RETURN"))
-	private float resetAttackCooldown(float original) {
-		return 1.0f;
+	private final float resetAttackCooldown(float original) {
+		return isAxe() ? original + 0.75f : 1.0f;
+	}
+
+	private final boolean isAxe() {
+		return MinecraftClient.getInstance().player.getMainHandStack().getItem() instanceof AxeItem;
 	}
 }
