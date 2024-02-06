4413046b09724d33536a80ca8723b6dc9055a971
Nikita Kurkurin <nkl@reshape.team>
2024-02-06 18:44:28 +0100

    master
    1) Refactoring;
    2) Apply no reset mod only for swords;

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 1.0.3 
+
+1) Add reduce attack cooldown only for swords;
+
+
 # 1.0.2
 
 1) Complete mod core.
--- src/main/java/minecraftlover/moonvent/RemoveAttackCooldown/mixin/AttackCooldownMixin.java
+++ src/main/java/minecraftlover/moonvent/RemoveAttackCooldown/mixin/AttackCooldownMixin.java
@@ -3,7 +3,7 @@ package minecraftlover.moonvent.RemoveAttackCooldown.mixin;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.AxeItem;
+import net.minecraft.item.SwordItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -11,11 +11,11 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(PlayerEntity.class)
 public class AttackCooldownMixin {
 	@ModifyReturnValue(method = "getAttackCooldownProgress", at = @At("RETURN"))
-	private final float resetAttackCooldown(float original) {
-		return isAxe() ? original + 0.75f : 1.0f;
+	private float resetAttackCooldown(float original) {
+		return isSword() ? 1.0f: original ;
 	}
 
-	private final boolean isAxe() {
-		return MinecraftClient.getInstance().player.getMainHandStack().getItem() instanceof AxeItem;
+	private boolean isSword() {
+		return MinecraftClient.getInstance().player.getMainHandStack().getItem() instanceof SwordItem;
 	}
 }
